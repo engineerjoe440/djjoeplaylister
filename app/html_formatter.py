3178208c88dee3655795586f019f5b5f77911d3a
Joe Stanley <engineerjoe440@yahoo.com>
2023-07-10 22:36:42 -0700

    further cleanup

--- app/html_formatter.py
+++ app/html_formatter.py
@@ -11,7 +11,6 @@ Spotify playlists.
 ################################################################################
 
 # Requirements
-import html
 from tabulate import tabulate
 
 YOUTUBE_BASE_URL = "https://www.youtube.com/results?search_query={}"
@@ -24,7 +23,7 @@ PLAYLIST_HTML = """
 """
 
 def format_youtube_search(terms: list):
-    # Generate a search-url for Youtube based on the terms provided.
+    """Generate a search-url for Youtube based on the terms provided."""
     query = ' '.join(terms).replace(' ', '%20')
     return YOUTUBE_BASE_URL.format(query)
 
@@ -42,23 +41,34 @@ def playlist_html_table(playlist: str, tracks: str):
     """Generate an HTML Table from the Playlist's Information."""
     table_list = []
     # Search for Text in Tracklist
-    for i, track in enumerate(tracks):
+    for track in tracks:
         # Apply Hyperlink
-        linked_title = f"""<div class="tracktitle"><a href="{format_youtube_search(track[:2])}" target="_blank"
-            rel="noopener noreferrer">{track[0]}</a></div>"""
+        linked_title = f"""
+            <div class="track-title">
+                <a
+                    href="{format_youtube_search(track[:2])}"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    {track[0]}
+                </a>
+            </div>"""
         # Identify Explicit Tracks
-        artist_marked_w_explicit = f"""<div class="artist">&nbsp&nbsp&nbsp-&nbsp&nbsp&nbsp{track[1]}</div>"""
+        artist_marked_w_explicit = f"""<div class="artist">{track[1]}</div>"""
         if track[2]:
-            artist_marked_w_explicit = f"""<div class="explicit">&nbsp&nbsp&nbsp-&nbsp&nbsp&nbsp{track[1]} (explicit)</div>"""
+            artist_marked_w_explicit = f"""
+            <div class="explicit">{track[1]} (explicit)</div>"""
         table_list.append(
             [linked_title, artist_marked_w_explicit]
         )
-    # Generate Table
-    table = tabulate(table_list, headers=["Title", "Artist(s)"], tablefmt="unsafehtml")
     # Generate the Inner HTML for the Table
     return PLAYLIST_HTML.format(
         playlist=playlist,
-        table=table
+        table=tabulate(
+            table_list,
+            headers=["Title", "Artist(s)"],
+            tablefmt="unsafehtml"
+        )
     )
 
 # END
